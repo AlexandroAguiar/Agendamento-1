a290afb0af904a6162430f26580b68a244a4d774
BrunoRoman <brunoroman05@gmail.com>
2020-11-26 17:59:14 -0300

    novas urls

--- src/main/java/br/com/romancorp/schedule/dao/AgendamentoDAO.java
+++ src/main/java/br/com/romancorp/schedule/dao/AgendamentoDAO.java
@@ -1,10 +1,14 @@
 package br.com.romancorp.schedule.dao;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.romancorp.schedule.model.Agendamento;
 
 public interface AgendamentoDAO extends CrudRepository<Agendamento, Integer> {
 	
-
+	public ArrayList<Agendamento> findAllByDataAgendamento(LocalDate data);
+	public ArrayList<Agendamento> findAllByNomeClienteContains(String nome);
 }
